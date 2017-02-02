f747186b44dda1ee4f718f5185e9911bf1a7f67a
Elizabeth O'Neill <elizabethoneill5678@gmail.com>
2017-02-02 17:28:02 -0500

    move all of hte non-drive subsystems and commands to steamworks:

---
+++ src/org/usfirst/frc/team342/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team342.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class ShooterSubsystem extends Subsystem{
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
