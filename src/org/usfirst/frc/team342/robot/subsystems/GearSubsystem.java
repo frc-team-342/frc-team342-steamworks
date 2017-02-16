505faeaf1d97eaa0b8592e4139a8e1c4b731418d
Elizabeth O'Neill <elizabethoneill5678@gmail.com>
2017-02-16 18:44:23 -0500

    moved GearSubsystem from dev-eo

---
+++ src/org/usfirst/frc/team342/robot/subsystems/GearSubsystem.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team342.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Relay.Direction;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class GearSubsystem extends Subsystem {
+	
+	private static final GearSubsystem instance= new GearSubsystem ();
+	private Relay DoorMotor;
+	
+	public void forward (){
+		DoorMotor.setDirection(Direction.kForward);
+	}
+	
+	public void backward (){
+		DoorMotor.setDirection(Direction.kReverse);
+	}
+	
+	public void stop (){
+		DoorMotor.stopMotor();
+	}
+	
+	public GearSubsystem (){
+		DoorMotor = new Relay (3);
+	
+	}
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+	public static GearSubsystem Getinstance (){
+		return instance;
+		
+	}
+}
+
