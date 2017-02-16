9e910e3c2663bcaa7314906e77780d99598c897d
Elizabeth O'Neill <elizabethoneill5678@gmail.com>
2017-02-16 18:37:21 -0500

    made changes to gear Subsystem

--- src/org/usfirst/frc/team342/robot/subsystems/GearSubsystem.java
+++ src/org/usfirst/frc/team342/robot/subsystems/GearSubsystem.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team342.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Relay.Direction;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class GearSubsystem extends Subsystem {
@@ -9,6 +10,17 @@ public class GearSubsystem extends Subsystem {
 	private static final GearSubsystem instance= new GearSubsystem ();
 	private Relay DoorMotor;
 	
+	public void forward (){
+		DoorMotor.setDirection(Direction.kForward);
+	}
+	
+	public void backward (){
+		DoorMotor.setDirection(Direction.kReverse);
+	}
+	
+	public void stop (){
+		DoorMotor.stopMotor();
+	}
 	
 	public GearSubsystem (){
 		DoorMotor = new Relay (3);
