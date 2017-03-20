272cadc09e9e445b3c2f797b82101ebf8734fd40
TheSaltyDog <hardlyfocus3d@gmail.com>
2017-03-20 17:45:52 -0400

    Welcome to my abode this is my code

---
+++ src/org/usfirst/frc/team342/robot/commands/DriveBackward.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team342.robot.commands;
+
+import org.usfirst.frc.team342.robot.subsystems.DriveSubsystem;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveBackward extends Command{
+	
+	private DriveSubsystem Drive;
+	private long Endtime;
+	private double Seconds;
+	
+	public DriveBackward(double seconds) {
+		Seconds = seconds;
+		Drive = DriveSubsystem.getInstance();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return (System.currentTimeMillis() > Endtime);
+	}
+	
+	protected void execute(){
+		Drive.DWJmanup(0.5, 0.5, 0.0, true);
+	}
+	
+	protected void end(){
+		Drive.DWJmanup(0, 0, 0, true);
+	}
+	
+	protected void interupted() {
+		end();
+	}
+	
+	protected void intialize(){
+		int MillisTime = (int) (Seconds * 1000.0);
+		Endtime = (System.currentTimeMillis() + MillisTime);
+		
+	}
+	
+}
