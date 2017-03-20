5a1d42958ca5ee8a575efe127b40faebded042ec
TheSaltyDog <hardlyfocus3d@gmail.com>
2017-03-20 18:17:58 -0400

    WORK! I Command It!

--- src/org/usfirst/frc/team342/robot/commands/AutoShootGroup.java
+++ src/org/usfirst/frc/team342/robot/commands/AutoShootGroup.java
@@ -3,10 +3,11 @@ package org.usfirst.frc.team342.robot.commands;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.command.CommandGroup;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class AutoShootGroup extends CommandGroup {
 
-	private static final double Time = 9.0;
+	private static final double Time = 6.0;
 	private static final double BlueBKSpeedSetting = 4000;
 	private static final double BlueFRSpeedSetting = 2300;
 	private static final double RedBKSpeedSetting = 4000;
@@ -15,6 +16,7 @@ public class AutoShootGroup extends CommandGroup {
 	public AutoShootGroup(){
 		boolean Blue = true;
 		DriverStation DS = DriverStation.getInstance();
+		SmartDashboard.putString("Alliance Color", DS.getAlliance().toString());
 		if(DS.getAlliance() == Alliance.Red){
 			Blue = false;
 		}
@@ -28,8 +30,8 @@ public class AutoShootGroup extends CommandGroup {
 				addSequential(new TimedManualShoot(RedFRSpeedSetting, RedBKSpeedSetting, Time));
 				addSequential(new DriveFoward(2.0));
 				
-				addSequential(new TimedManualShoot(RedFRSpeedSetting, RedBKSpeedSetting, Time));
-				addSequential(new DriveFoward(2.0));
+				//addSequential(new TimedManualShoot(RedFRSpeedSetting, RedBKSpeedSetting, Time));
+				///addSequential(new DriveFoward(2.0));
 			}
 		
 		
